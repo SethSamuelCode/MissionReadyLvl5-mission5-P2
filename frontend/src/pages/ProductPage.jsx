bde932acb98d3626fa661bb15c928cad9257ef21
Seth Samuel <Seths@missionreadyhq.com>
2025-06-23 11:47:13 +1200

    Add ProductPage divs added css class names

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import styles from "./ProductPage.module.css";
 
 export default function ProductPage() {
   return (
-    <div>ProductPage</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.images}></div>
+      <div className={styles.sidebar}>
+        <div className={styles.title}></div>
+        <div className={styles.watchListAndCompareButton}></div>
+        <div className={styles.bidBox}></div>
+        <div className={styles.bidHistory}></div>
+      </div>
+      <div className={styles.shippingAndPaymentOptions}>
+        <div className={styles.shippingAndPickup}></div>
+        <div className={styles.paymentOptions}></div>
+      </div>
+      <div className={styles.productDetailsAndDescription}></div>
+      <div className={styles.sellerContainer}>
+        <div className={styles.sellerInfo}></div>
+        <div className={styles.sellerLogo}></div>
+      </div>
+      <div className={styles.questionsAndAnswers}></div>
+      <div className={styles.similarItems}></div>
+    </div>
+  );
 }
