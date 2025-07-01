167fbd7b4a5aaf0a697618de40582f7964c62b99
Valentine Ncube <valentinemelly77@gmail.com>
2025-07-02 00:44:51 +1200

    added sidebar

--- frontend/src/pages/Watchlist.jsx
+++ frontend/src/pages/Watchlist.jsx
@@ -31,6 +31,30 @@ const dummyItems = [
   },
 ]
 
-function Watchlist() {}
+function Watchlist() {
+  return (
+    <div className={styles.pageWrapper}>
+      <Header />
+
+      <main className={styles.mainContent}>
+        <aside className={styles.sidebar}>
+          <ul className={styles.sidebarList}>
+            <li>Account Details</li>
+            <li>Notifications</li>
+            <li className={styles.active}>Watchlist</li>
+            <li>Favorites</li>
+            <li>Fixed Price offer</li>
+            <li>Start Listing</li>
+            <li>Items I'm Listing</li>
+            <li>Sold</li>
+            <li>Unsold</li>
+            <li>Sales Summary</li>
+            <li>My Products</li>
+          </ul>
+        </aside>
+      </main>
+    </div>
+  )
+}
 
 export default Watchlist
