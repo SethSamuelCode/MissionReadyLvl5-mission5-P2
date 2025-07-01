4ae974edb63883167730a542f9c1b9f410ed2d39
Valentine Ncube <valentinemelly77@gmail.com>
2025-07-01 21:15:10 +1200

    accepted incoming ProductPage css

--- frontend/src/pages/Watchlist.jsx
+++ frontend/src/pages/Watchlist.jsx
@@ -1,9 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import styles from '../styles/Watchlist.module.css'
+import React from 'react'
+import styles from './Watchlist.module.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
 
-const Watchlist = () => {
-  const [watchlistItems, setWatchlistItems] = useState([])
-  const navigate = useNavigate()
-}
+const dummyItems = [
+  {
+    id: 1,
+    title: 'Double bed 1',
+    description: 'Description',
+    location: 'North shore',
+    closingTime: 'Fri 13th July, 4:19pm',
+    price: '$1,200',
+  },
+  {
+    id: 2,
+    title: 'Double bed 2',
+    description: 'Description',
+    location: 'North shore',
+    closingTime: 'Fri 13th July, 4:19pm',
+    price: '$800',
+  },
+  {
+    id: 3,
+    title: 'Double bed 3',
+    description: 'Description',
+    location: 'North shore',
+    closingTime: 'Fri 13th July, 4:19pm',
+    price: '$1,100',
+  },
+]
+
+function Watchlist() {}
+
+export default Watchlist
